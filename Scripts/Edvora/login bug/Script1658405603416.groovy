a66044fc2b83bca59612993ba150d5ecea10f7dc
Ammar <76597039+merotatox@users.noreply.github.com>
2022-07-21 14:15:10 +0200

    Add files via upload

---
+++ Scripts/Edvora/login bug/Script1658405603416.groovy
@@ -0,0 +1,45 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.testobject.SelectorMethod
+
+import com.thoughtworks.selenium.Selenium
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.WebDriver
+import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium
+import static org.junit.Assert.*
+import java.util.regex.Pattern
+import static org.apache.commons.lang3.StringUtils.join
+import org.testng.asserts.SoftAssert
+import com.kms.katalon.core.testdata.CSVData
+import org.openqa.selenium.Keys as Keys
+
+SoftAssert softAssertion = new SoftAssert();
+WebUI.openBrowser('https://www.google.com/')
+def driver = DriverFactory.getWebDriver()
+String baseUrl = "https://www.google.com/"
+selenium = new WebDriverBackedSelenium(driver, baseUrl)
+selenium.open("https://testing-assessment-foh15kew9-edvora.vercel.app/")
+selenium.click("xpath=//input[@value='']")
+selenium.type("xpath=//div[@id='__next']/div/main/div/div/div/div/input", "scdfs")
+selenium.click("xpath=//input[@value='']")
+selenium.type("xpath=//div[@id='__next']/div/main/div/div/div[2]/div/input", "sdfsdf")
+selenium.click("xpath=//button[@type='button']")
---
+++ Test Suites/Edvora.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Edvora</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fda22dc7-bd8b-42bb-8ff1-fdc14f4508a0</testSuiteGuid>
+   <testCaseLink>
+      <guid>15e655c7-d420-4245-9e07-87270f13bebf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Edvora/Signup</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>578f15db-b94b-42b2-9084-36a8bd62c7e3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Edvora/login</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>a17b08b5-d851-43bf-b58e-01ebedbfc4a3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Edvora/settings profile</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>116dba10-52a9-4394-afc7-6824870dc47f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Edvora/settings security</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>051f14e3-feeb-4df1-a228-75b998948a7e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Edvora/settings logout</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>679ec2ea-0dc1-4fa0-9b10-9648b9c360d1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Edvora/login bug</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
